6989c7b24e91fc654dbc30652ac68452cd9968ee
Sharifah <sharifah@besquare.com.my>
2022-07-11 23:46:02 +0800

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -18,11 +18,33 @@ export default function App() {
     setColor(event.target.value);
   };
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(event);
+  };
+
+  console.log(React);
+
+  // function handleClick() {
+  //   alert("Hi");
+  // }
+
   return (
-    <div>
-      <Input name="Name" value={name} onChange={changeName} />
-      <Input name="Age" value={age} onChange={changeAge} />
-      <Select name="Favourite Color" value={color} onChange={changeColor} />
+    <div className="container">
+      <h1>Fill the form!</h1>
+      <form onSubmit={handleSubmit} className="h-center w-50">
+        <Input name="Name" value={name} onChange={changeName} />
+        <Input name="Age" value={age} onChange={changeAge} />
+        <Select name="Favourite Color" value={color} onChange={changeColor} />
+        <button type="submit" className="btn-success">
+          Submit
+        </button>
+        <br></br>
+        {/* <button onClick={handleClick}>Clear</button> */}
+        <button type="reset" className="btn-secondary">
+          Reset
+        </button>
+      </form>
     </div>
   );
 }
@@ -52,8 +74,19 @@ const Select = ({ value, onChange, name }: SelectType) => {
   return (
     <div className="pa-16">
       <label>{name}</label>
-      <select value={value} onSelect={onChange}>
+
+      <select value={value} onSelect={onChange} className="w-100">
         <option value="red">Red</option>
+        <option value="red">Blue</option>
+        <option value="red">Yellow</option>
+        <option value="red">Orange</option>
+        <option value="skyblue">Skyblue</option>
+        <option value="green">Green</option>
+        <option value="pink">Pink</option>
+        <option value="pink">Cyan</option>
+        <option value="pink">Grey</option>
+        <option value="pink">Black</option>
+        <option value="pink">White</option>
       </select>
     </div>
   );
